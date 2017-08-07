f590aa98f2533253de91760808ab3081428c055a
fwchen <crystal.tyan@gmail.com>
2017-08-07 21:35:30 +0800

    intial commit

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Foundation
 
 extension Droplet {
     func setupRoutes() throws {
@@ -19,7 +20,18 @@ extension Droplet {
         }
 
         get("description") { req in return req.description }
-        
+
         try resource("posts", PostController.self)
+
+        let filePath = "./data.txt"
+        let handler = FileHandle(forWritingAtPath: filePath)
+        let string = "添加一些文字到文件末尾"
+        let appendedData = string.data(using: String.Encoding.utf8, allowLossyConversion: true)
+        handler?.seekToEndOfFile()
+        handler?.write(appendedData!)
+
+
+        // let fileContents = try? String(contentsOfFile: "./fileName.txt", encoding: String.Encoding.utf8)
+        // print(fileContents) // 输出字符串可选类型
     }
 }
